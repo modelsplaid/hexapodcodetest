0cfa26319fca159e2c208a9fa65602aae0552745
modelsplaid <modelsplaid@qq.com>
2022-06-11 13:29:17 +0800

    json2db now works

--- learnsqlite/json2db/servo_json_2db.py
+++ learnsqlite/json2db/servo_json_2db.py
@@ -15,7 +15,12 @@ class servoCommuJson2DB:
         return data_json
 
     def conver_json_2_db(self,json_file= "recorded_servo_data.json",db_file = 'example.db'):
+        
         self.connect(db_file)
+        try:
+            self.execute_create_table()
+        except:
+            print("table already exit,insert to present db")
         jsondata = self.load_servo_json_recoredr(json_file)
         num_frams = len(jsondata)
         for i in range(num_frams):
@@ -27,7 +32,7 @@ class servoCommuJson2DB:
                 one_dev_data = keyvalpair[j][1] # get the value
                 #print("one_dev_data:"+str(one_dev_data) )
                 self.execute_insert_into(one_dev_data)
-                return
+                
 
     def execute_create_table(self):
         # Create table
@@ -50,31 +55,71 @@ class servoCommuJson2DB:
                        time_stamp real               
                        )''')
     
+    def jsonbool2dbint(self,json_bool):
+        if(json_bool==True):
+            return str(1)
+        else:
+            return str(0)
+        
+
     def execute_insert_into(self,one_json_data):
         # Insert a row of data
         str_device_id = str(one_json_data["device_id"])
-        str_send_servo_valid = str(one_json_data["send_servo_valid"])# todo here
-        str_send_servo_pos_val = str(one_json_data["send_servo_pos_val"])
-        
-        str_send_servo_pos_stats = str(one_json_data["send_servo_pos_stats"]) # todo here
-
+        str_send_servo_valid = self.jsonbool2dbint(str(one_json_data["send_servo_valid"]))
+        str_send_servo_pos_val = str(one_json_data["send_servo_pos_val"])        
+        str_send_servo_pos_stats = "'"+ one_json_data["send_servo_pos_stats"]+"'"
         str_send_servo_speed_val = str(one_json_data["send_servo_speed_val"])
-        str_send_servo_speed_stats = str(one_json_data["send_servo_speed_stats"]) # todo here
+        str_send_servo_speed_stats = "'"+one_json_data["send_servo_speed_stats"]+"'"
         str_send_servo_torque_val = str(one_json_data["send_servo_torque_val"])
-        str_send_servo_torque_stats = str(one_json_data["send_servo_torque_stats"])# todo here
-        str_recv_servo_valid = str(one_json_data["recv_servo_valid"])# todo here
+        str_send_servo_torque_stats = "'"+one_json_data["send_servo_torque_stats"]+"'"
+        str_recv_servo_valid = self.jsonbool2dbint(str(one_json_data["recv_servo_valid"]))
         str_recv_servo_pos_val = str(one_json_data["recv_servo_pos_val"])
-        str_recv_servo_pos_stats = str(one_json_data["recv_servo_pos_stats"])# todo here
+        str_recv_servo_pos_stats = "'"+one_json_data["recv_servo_pos_stats"]+"'"
         str_recv_servo_speed_val = str(one_json_data["recv_servo_speed_val"])
-        str_recv_servo_speed_stats = str(one_json_data["recv_servo_speed_stats"])# todo here
+        str_recv_servo_speed_stats = "'"+one_json_data["recv_servo_speed_stats"]+"'"
         str_recv_servo_torque_val = str(one_json_data["recv_servo_torque_val"])
-        str_recv_servo_torque_stats = str(one_json_data["recv_servo_torque_stats"])# todo here
+        str_recv_servo_torque_stats = "'"+one_json_data["recv_servo_torque_stats"]+"'"
         str_time_stamp = str(one_json_data["time_stamp"])
 
-        device_id = 7
+
+        #self.cur.execute("INSERT INTO serial_servo_commu VALUES ("+str_device_id+ ",1,1200,'good',200,'good',500,'good',1,1200,'good',200,'good',500,'good',12.3)")
+        
+        print("INSERT INTO serial_servo_commu VALUES ("+
+            str_device_id+","+
+            str_send_servo_valid+","+
+            str_send_servo_pos_val+","+
+            str_send_servo_pos_stats+","+
+            str_send_servo_speed_val+","+
+            str_send_servo_speed_stats+","+
+            str_send_servo_torque_val+","+
+            str_send_servo_torque_stats+","+
+            str_recv_servo_valid+","+
+            str_recv_servo_pos_val+","+
+            str_recv_servo_pos_stats+","+
+            str_recv_servo_speed_val+","+
+            str_recv_servo_speed_stats+","+
+            str_recv_servo_torque_val+","+
+            str_recv_servo_torque_stats+","+
+            str_time_stamp+")")
 
         self.cur.execute("INSERT INTO serial_servo_commu VALUES ("+
-            str(device_id)+ ",1,1200,'good',200,'good',500,'good',1,1200,'good',200,'good',500,'good',12.3)")
+            str_device_id+","+
+            str_send_servo_valid+","+
+            str_send_servo_pos_val+","+
+            str_send_servo_pos_stats+","+
+            str_send_servo_speed_val+","+
+            str_send_servo_speed_stats+","+
+            str_send_servo_torque_val+","+
+            str_send_servo_torque_stats+","+
+            str_recv_servo_valid+","+
+            str_recv_servo_pos_val+","+
+            str_recv_servo_pos_stats+","+
+            str_recv_servo_speed_val+","+
+            str_recv_servo_speed_stats+","+
+            str_recv_servo_torque_val+","+
+            str_recv_servo_torque_stats+","+
+            str_time_stamp+")"
+            )
     
     def commit(self):
         self.con.commit()
