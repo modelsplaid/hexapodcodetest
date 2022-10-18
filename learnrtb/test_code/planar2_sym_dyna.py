a86953149602ddbff228d71c2a0cce2ee8633f19
modelsplaid <modelsplaid@qq.com>
2022-10-18 18:30:23 +0800

    todo veriy inward dyna for two link robot

--- learnrtb/test_code/planar2_sym_dyna.py
+++ learnrtb/test_code/planar2_sym_dyna.py
@@ -47,28 +47,32 @@ class Planar2(DHRobot):
         a1, a2 = sym.symbol("l1 l2")  # type: ignore
         a0 = zero
 
-        r0 = [a1, zero, zero] # center of mass position with respect to ith link
-        r1 = [a2, zero, zero] # center of mass position with respect to ith link
-        r2 = [zero, zero, zero] # center of mass position with respect to ith link
+        rl1, rl2 = sym.symbol("rl1 rl2")
+        r0 = [zero, zero, zero] # center of mass position with respect to ith link
+        r1 = [rl1, zero, zero] # center of mass position with respect to ith link
+        r2 = [rl2, zero, zero] # center of mass position with respect to ith link
 
         m1, m2, = sym.symbol("m1 m2")  # type: ignore
         m3 = zero
+        m0 = zero
 
+        I0=[zero, zero, zero, zero, zero, zero]
         I1=[zero, zero, zero, zero, zero, zero]
         I2=[zero, zero, zero, zero, zero, zero]
         I3=[zero, zero, zero, zero, zero, zero]
 
         
         L = [\
-            RevoluteMDH(a=a0, alpha=zero,r=r0,m=m1,I=I1,Jm=zero,G=zero,B=zero,Tc=[zero,zero]),\
-            RevoluteMDH(a=a1, alpha=zero,r=r1,m=m2,I=I2,Jm=zero,G=zero,B=zero,Tc=[zero,zero]),\
-            RevoluteMDH(a=a2, alpha=zero,r=r2,m=m3,I=I3,Jm=zero,G=zero,B=zero,Tc=[zero,zero])\
+            RevoluteMDH(a=a0, alpha=zero,r=r0,m=m0,I=I0,Jm=zero,G=zero,B=zero,Tc=[zero,zero]),\
+            RevoluteMDH(a=a0, alpha=zero,r=r1,m=m1,I=I1,Jm=zero,G=zero,B=zero,Tc=[zero,zero]),\
+            RevoluteMDH(a=a1, alpha=zero,r=r2,m=m2,I=I2,Jm=zero,G=zero,B=zero,Tc=[zero,zero]),\
+            RevoluteMDH(a=a2, alpha=zero,Jm=zero,G=zero,B=zero,Tc=[zero,zero])
             ]
 
         super().__init__(L, symbolic=True,name="Planar 2 link", keywords=("planar",))
 
-        self.qr = [pi/4, pi/4, pi/4]
-        self.qz = [zero, zero, zero]
+        self.qr = [pi/4, pi/4, pi/4,zero]
+        self.qz = [zero, zero, zero,zero]
 
         self.addconfiguration("qr", self.qr)
         self.addconfiguration("qz", self.qz)
@@ -78,10 +82,12 @@ class Planar2(DHRobot):
         lk1sym=L[0].A(q=theta1)
         lk2sym=L[1].A(q=theta2)
         lk3sym=L[2].A(q=sym.zero())
+        lk4sym=L[3].A(q=sym.zero())
         
         print("0_1_T: \n"+str(lk1sym))
         print("1_2_T: \n"+str(lk2sym))
         print("2_3_T: \n"+str(lk3sym))
+        print("3_4_T: \n"+str(lk4sym))
 
 def symbolic_dyna():
 
@@ -92,9 +98,12 @@ def symbolic_dyna():
     pi = sym.pi()
     grav_g = base.sym.symbol('g')
 
-    thetas = [theta1,theta2,zero]
-    thetads = [thetad1,thetad2,zero]
-    thetadds = [thetadd1,thetadd2,zero]
+    thetas = [zero,theta1,theta2,zero]
+    thetads = [zero,thetad1,thetad2,zero]
+    thetadds = [zero,thetadd1,thetadd2,zero]
+    #thetas =    [theta1,theta2,zero]
+    #thetads =   [thetad1,thetad2,zero]
+    #thetadds =  [thetadd1,thetadd2,zero]
     gravs = [zero,grav_g,zero]
 
 
@@ -108,6 +117,7 @@ def symbolic_dyna():
     print("simpdynamat:\n")
     for i in range(len(simpdynamat)):
         print(simpdynamat[i])
+
 def symbolic_jacob():
     
 
