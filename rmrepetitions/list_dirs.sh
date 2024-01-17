80409d3af6a91cc3b10d7485782841948fca1668
modelsplaid <modelsplaid@qq.com>
2024-01-17 18:10:55 +0800

    done search dirs in same path. todo: work on automatic remove duplicate

--- rmrepetitions/list_dirs.sh
+++ rmrepetitions/list_dirs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 get_folder_lst() {
+
     # Check if a path is provided
     if [ -z "$1" ]
     then
@@ -10,39 +11,53 @@ get_folder_lst() {
         search_path=$1
 
     fi
+
     #declare -g found_lst
     # List all folders in the provided path
     declare -g directories=()
-
     # Use while loop and read to correctly handle directory names with spaces
     while IFS= read -r dir; do
         directories+=("$dir")  # Append each directory to the array
-    done < <(find "$search_path" -type d)
+    done < <(find "$search_path" -maxdepth 1 -type d)
+
+    # Remove the first element
+    unset directories[0] # Remove the first element
+    directories=("${directories[@]}") # Re-index the array
 
     # Now you can iterate over the array and access each directory name
-    for dir in "${directories[@]}"; do
-        echo "$dir"
-    done
+    # for dir in "${directories[@]}"; do
+    #     echo "$dir"
+    # done
 
 }
 
-get_folder_lst
+loop_folder_lst() {
+    directories=$1
+    dirs_arr=("${directories[@]}")
 
+    # Get the length of the array
+    array_length=${#dirs_arr[@]}
+    echo Found number of directories: $array_length
 
-# Example array
-my_array=("${directories[@]}")
+    # Iterate over the array
+    for (( i=0; i<array_length; i++ )); do
+        # Print the current state of the array
 
-# Get the length of the array
-array_length=${#my_array[@]}
-echo array_length: $array_length
+        # Get first dir
+        fst_dir=$dirs_arr
 
-# Iterate over the array
-for (( i=0; i<array_length; i++ )); do
-    # Print the current state of the array
-    #echo "${my_array[@]}"
-    echo "${my_array[@]}"
+        # Remove the first element
+        unset dirs_arr[0] # Remove the first element
+        dirs_arr=("${dirs_arr[@]}") # Re-index the array
+    
+        # loop over remain 
+        for one_dir in ${dirs_arr[@]}; do
+            echo Compare: $fst_dir with: $one_dir
+            bash rmrep.sh $fst_dir $one_dir
+        done
 
-    # Remove the first element
-    unset my_array[0] # Remove the first element
-    my_array=("${my_array[@]}") # Re-index the array
-done
+    done
+}
+
+get_folder_lst $1
+loop_folder_lst $directories
