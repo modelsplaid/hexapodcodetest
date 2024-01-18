617d765a569f73eaaaba0b36a896e482fc102589
modelsplaid <modelsplaid@qq.com>
2024-01-18 11:24:53 +0800

    rmrep.sh and list_dirs.sh

--- rmrepetitions/list_dirs.sh
+++ rmrepetitions/list_dirs.sh
@@ -52,7 +52,7 @@ loop_folder_lst() {
     
         # loop over remain 
         for one_dir in ${dirs_arr[@]}; do
-            echo Compare: $fst_dir with: $one_dir
+            #echo Compare: $fst_dir with: $one_dir
             bash rmrep.sh $fst_dir $one_dir
         done
 
