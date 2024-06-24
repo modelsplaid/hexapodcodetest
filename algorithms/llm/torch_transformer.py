61317bec5980b352fcdb3aa0e55ab1b665784f16
modelsplaid <modelsplaid@qq.com>
2024-06-24 10:36:38 +0800

    upds

--- algorithms/llm/torch_transformer.py
+++ algorithms/llm/torch_transformer.py
@@ -178,33 +178,39 @@ class Transformer(nn.Module):
 
         output = self.fc(dec_output)
         return output
+    
 
+def test1():
 
-src_vocab_size = 5000
-tgt_vocab_size = 5000
-d_model = 512
-num_heads = 8
-num_layers = 6
-d_ff = 2048
-max_seq_length = 100
-dropout = 0.1
+    src_vocab_size = 5000
+    tgt_vocab_size = 5000
+    d_model = 512
+    num_heads = 8
+    num_layers = 6
+    d_ff = 2048
+    max_seq_length = 100
+    dropout = 0.1
 
-transformer = Transformer(src_vocab_size, tgt_vocab_size, d_model, num_heads, num_layers, d_ff, max_seq_length, dropout)
+    transformer = Transformer(src_vocab_size, tgt_vocab_size, d_model, num_heads, num_layers, d_ff, max_seq_length, dropout)
 
-# Generate random sample data
-#                   start_min   end_max      row   colum
-src_data = torch.randint(1, src_vocab_size, (64, max_seq_length))  # (batch_size, seq_length)
-tgt_data = torch.randint(1, tgt_vocab_size, (64, max_seq_length))  # (batch_size, seq_length)
+    # Generate random sample data
+    #                   start_min   end_max      row   colum
+    src_data = torch.randint(1, src_vocab_size, (64, max_seq_length))  # (batch_size, seq_length)
+    tgt_data = torch.randint(1, tgt_vocab_size, (64, max_seq_length))  # (batch_size, seq_length)
 
-criterion = nn.CrossEntropyLoss(ignore_index=0)
-optimizer = optim.Adam(transformer.parameters(), lr=0.0001, betas=(0.9, 0.98), eps=1e-9)
+    criterion = nn.CrossEntropyLoss(ignore_index=0)
+    optimizer = optim.Adam(transformer.parameters(), lr=0.0001, betas=(0.9, 0.98), eps=1e-9)
 
-transformer.train()
+    transformer.train()
 
-for epoch in range(1):
-    optimizer.zero_grad()
-    output = transformer(src_data, tgt_data[:, :-1])
-    loss = criterion(output.contiguous().view(-1, tgt_vocab_size), tgt_data[:, 1:].contiguous().view(-1))
-    loss.backward()
-    optimizer.step()
-    print(f"Epoch: {epoch+1}, Loss: {loss.item()}")
+    for epoch in range(1):
+        optimizer.zero_grad()
+        output = transformer(src_data, tgt_data[:, :-1])
+        loss = criterion(output.contiguous().view(-1, tgt_vocab_size), tgt_data[:, 1:].contiguous().view(-1))
+        loss.backward()
+        optimizer.step()
+        print(f"Epoch: {epoch+1}, Loss: {loss.item()}")
+        
+        
+if __name__ == "__main__":
+    print("a")
